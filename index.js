47cc7d74776498bf9232e767fad05c92a8495d52
Chance Hudson <jchancehud@gmail.com>
2017-05-11 21:40:16 -0500

    Added support for passing a function as first arg

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ const path = require('path');
 const map = require('lodash.map');
 const filter = require('lodash.filter');
 
-function notIndex(filenames = [], regex = /^(?!index)[a-z\-]+\.js$/) {
+function notIndex(filenames = [], fn = i => i) {
+  const regex = /^(?!index)[a-z\-]+\.js$/;
   let output = filenames;
   output = filter(filenames, filename => regex.test(filename));
   output = map(output, filename => path.join(__dirname, filename));
   output = map(output, filepath => require(filepath));
+  output = map(output, fn);
   return output;
 }
 
-function _notIndex(regex) {
-  return notIndex(fs.readdirSync(__dirname), regex);
+function _notIndex(fn) {
+  return notIndex(fs.readdirSync(__dirname), fn);
 }
 
-_notIndex.promise = (regex) => {
+_notIndex.promise = fn => {
   return new Promise((resolve, reject) => {
     fs.readdir(__dirname, (err, filenames) => {
       if (err) reject(err);
-      else resolve(notIndex(filenames, regex));
+      else resolve(notIndex(filenames, fn));
     });
   });
 };
