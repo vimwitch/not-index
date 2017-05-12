3a8b0168c0912d2ad318a8d443e2f13936955660
Chance Hudson <jchancehud@gmail.com>
2017-05-11 20:32:32 -0500

    Added notIndex function Added support for promise chaining with .then

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+
+function notIndex(filenames = [], regex = /^(?!index)[a-z\-]+\.js$/) {
+  return _.chain(filenames)
+    .filter(filename => regex.test(filename))
+    .map(filename => path.join(__dirname, filename))
+    .map(filepath => require(filepath))
+    .value();
+}
+
+function _notIndex(regex) {
+  return notIndex(fs.readdirSync(__dirname), regex);
+}
+
+_notIndex.then = (regex) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(__dirname, (err, filenames) => {
+      if (err) reject(err);
+      else resolve(notIndex(filenames, regex));
+    });
+  });
+};
+
+module.exports = _notIndex;
