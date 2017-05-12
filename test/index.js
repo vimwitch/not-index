e6ab88697ec2df18ed569550fb7e6ef47de9cc6e
Chance Hudson <jchancehud@gmail.com>
2017-05-11 22:31:49 -0500

    Added test message

--- test/index.js
+++ test/index.js
@@ -4,9 +4,10 @@ test(require('../')(__dirname));
 
 require('../')
   .promise(__dirname)
-  .then(test)
-  .catch(err => console.log(`Error in async test`, err));
+  .then(test);
 
 function test(requires = []) {
   if (requires.length !== 3) throw new Error('Invalid count');
 }
+
+console.log('Everything seems to have worked');
