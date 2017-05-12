abd25a3eb039b5d87281553eb981e9cd1fc06bfc
Chance Hudson <jchancehud@gmail.com>
2017-05-11 22:29:59 -0500

    Added simple tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const requires = require('../')(__dirname);
+test(require('../')(__dirname));
 
-if (requires.length !== 3) throw new Error('Invalid count');
+require('../')
+  .promise(__dirname)
+  .then(test)
+  .catch(err => console.log(`Error in async test`, err));
+
+function test(requires = []) {
+  if (requires.length !== 3) throw new Error('Invalid count');
+}
