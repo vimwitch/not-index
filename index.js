b6e0d99c51e4b18e69af11d76c79bd46c9785f82
Chance Hudson <jchancehud@gmail.com>
2017-05-11 22:06:33 -0500

    Reverted regex logic, did not like the map functionality

--- index.js
+++ index.js
@@ -5,25 +5,23 @@ const path = require('path');
 const map = require('lodash.map');
 const filter = require('lodash.filter');
 
-function notIndex(filenames = [], fn = i => i) {
-  const regex = /^(?!index)[a-z\-]+\.js$/;
+function notIndex(filenames = [], regex = /^(?!index)[a-z\-]+\.js$/) {
   let output = filenames;
   output = filter(filenames, filename => regex.test(filename));
   output = map(output, filename => path.join(__dirname, filename));
   output = map(output, filepath => require(filepath));
-  output = map(output, fn);
   return output;
 }
 
-function _notIndex(fn) {
-  return notIndex(fs.readdirSync(__dirname), fn);
+function _notIndex(regex) {
+  return notIndex(fs.readdirSync(__dirname), regex);
 }
 
-_notIndex.promise = fn => {
+_notIndex.promise = regex => {
   return new Promise((resolve, reject) => {
     fs.readdir(__dirname, (err, filenames) => {
       if (err) reject(err);
-      else resolve(notIndex(filenames, fn));
+      else resolve(notIndex(filenames, regex));
     });
   });
 };
